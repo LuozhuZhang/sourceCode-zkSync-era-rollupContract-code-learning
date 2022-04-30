6b38c6c7cbce97490995d5d9d90e9ded3ecc47bc
Mickey <hedgefund996@gmail.com>
2022-04-30 19:59:29 +0800

    Ownable.sol：Fix some solidity problems

--- contracts/contracts/Ownable.sol
+++ contracts/contracts/Ownable.sol
@@ -17,6 +17,10 @@ contract Ownable {
 
     /// @notice Check if specified address is master
     /// @param _address Address to check
+    // 检查调用的地址是否是master（owner），这个function被许多contract调用使用
+    // view（v0.5中定义），只读不写
+    // 好奇onlyOwner就可以，为什么要写的这么复杂呢？这个问题在阅读openzepplien源码时被解答：
+    // onlyOwner并不是solidity native的，而是op中的basic contract，zksync在这里做了完全相同的事情
     function requireMaster(address _address) internal view {
         require(_address == getMaster(), "1c"); // oro11 - only by master
     }
@@ -43,6 +47,8 @@ contract Ownable {
     /// @param _newMaster New masters address
     function transferMastership(address _newMaster) external {
         requireMaster(msg.sender);
+        // 向0地址转账会创建一个新的contract，owner transfer到0 addr会发生什么呢？返回false还是产生更复杂的交易。这个问题也在阅读OZ源码时被解答 ⬇
+        // 将owner转移到0，则放弃contract的所有权（contract不会被删除，但是没有所有者）
         require(_newMaster != address(0), "1d"); // otp11 - new masters address can't be zero address
         setMaster(_newMaster);
     }
